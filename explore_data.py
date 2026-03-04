19d51c2cb349782f3e4dbbfd2ce49046e29f2fa5
Alejandro De La Lama <delalama.7@gmail.com>
2026-03-04 14:44:52 -0600

    Filter positions to fantasy relevant, group by player and season, aggregate weekly score to season totals

--- explore_data.py
+++ explore_data.py
@@ -24,6 +24,12 @@ players_clean = players[players['season_type'] == "REG"]
 players_clean = players_clean[['player_id', 'player_display_name', 'position', 'season',
                                'week', 'team', 'fantasy_points_ppr']]
 
-print(players_clean.shape)
-print(players_clean.head())
+# print(players_clean.shape)
+# print(players_clean.head())
 
+position_filter = players_clean[players_clean['position'].isin(['QB', 'K', 'WR', 'RB', 'TE'])]
+
+season_totals = position_filter.groupby(['player_id', 'player_display_name', 'position', 'season', 'team'])[
+    'fantasy_points_ppr'].sum().reset_index()
+
+print(season_totals.head())
