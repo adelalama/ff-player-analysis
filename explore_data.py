906ba89a0603d118588d48dd5085885ce2dab300
Alejandro De La Lama <delalama.7@gmail.com>
2026-03-04 21:44:59 -0600

    Filter season totals to starters per position in fantasy QB:12, RB:24, WR:24, TE:12

--- explore_data.py
+++ explore_data.py
@@ -1,35 +1,27 @@
 import nflreadpy as nfl
+import pandas as pd
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 200)
 
 seasons = [2019, 2020, 2021, 2022, 2023]
+position_starters = {'QB': 12, 'RB': 24, 'WR': 24, 'TE': 12}
 
+# Data loading
 players = nfl.load_player_stats(seasons)
 players = players.to_pandas()
 
-# print(players.shape)
-# print(players.head())
-# print(players.columns)
-# # print(players['season'])
-# # # solid_players = players['fantasy_points'] > 25
-# # # print(solid_players)
-# #
-# print(players[players['fantasy_points']>25])
-
-# print(players['season'].unique())
-
-# print(players.columns)
-# print(players['season_type'].head())
-
-
+# data cleaning
 players_clean = players[players['season_type'] == "REG"]
 players_clean = players_clean[['player_id', 'player_display_name', 'position', 'season',
                                'week', 'team', 'fantasy_points_ppr']]
+position_filter = players_clean[players_clean['position'].isin(['QB', 'WR', 'RB', 'TE'])]
 
-# print(players_clean.shape)
-# print(players_clean.head())
-
-position_filter = players_clean[players_clean['position'].isin(['QB', 'K', 'WR', 'RB', 'TE'])]
-
+# Data transformation
 season_totals = position_filter.groupby(['player_id', 'player_display_name', 'position', 'season', 'team'])[
     'fantasy_points_ppr'].sum().reset_index()
 
-print(season_totals.head())
+season_totals['rank'] = season_totals.groupby(['season', 'position'])['fantasy_points_ppr'].rank(ascending=False)
+
+fantasy_starters = season_totals[season_totals['rank'] <= season_totals['position'].map(position_starters)]
+print(fantasy_starters.shape)
